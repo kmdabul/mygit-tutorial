bc2907cff30baee3c79ae05864bfc2f51c51ef14
kmdabul <kmdabul570@gmail.com>
2021-03-22 17:16:54 +0600

    Added a new method called display to Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,13 @@
 public class Hello {
     public static void main(string[] args) {
         System.out.println("Vs Code is Awesome...");
+
+
+        greeting("Moh Rohan Ahmed");
+    }
+
+
+    public static void greeting(string name){
+        System.out.println("welcome" + name);
     }
 }
